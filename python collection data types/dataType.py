a9893049a37b5d495ad45711fea23ee5040e1f61
IIK99 <muhamad.ikmal1099@gmail.com>
2024-05-22 16:05:06 +0700

    data type

---
+++ python collection data types/dataType.py
@@ -0,0 +1,15 @@
+# list
+list = [1, 2, 3, "hello python", True]
+print(list, type(list))
+
+# tuple list
+tuple = (1, 2, 3, "hello python", True)
+print(tuple, type(tuple))
+
+# set list
+set = {1, 1, 2, 2, 3, 3, "hello python", True}
+print(set, type(set))
+
+# dictionary
+dictionary = {"name": "Joan", "age": 25}
+print(dictionary, type(dictionary))
